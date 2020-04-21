771aa50905b45e9c9ecf9cc6f2d271c182de7bec
Mike Redko <30830926+somerandomdev49@users.noreply.github.com>
2020-04-21 20:07:07 +0300

    Hmm

---
+++ README.md
@@ -0,0 +1,77 @@
+# Turtle Master 2200
+
+To get help for the command line interface: `python main.py help`
+
+## Values:
+each value, string, anything (except for instruction names) is a value. And because those values have different types, the Turtle Master needs to be able to differentiate between those types. So here are *prefixes* that depict what type the value is:
+ *prefix*   | type   | description 
+------------|--------|-------------
+ `$`        | number | signed float
+ `%`        | data   | value of a data entry with a name
+ `[string]` | string | string with `\n \r \t` escapes.
+ `=`        | name   | returns the *name* of an entry
+ `@`        | last   | no value, just the prefix, last compute value
+
+## Instruction set:
+```
+forward:x go forward by x
+backward:x go backward by x
+left:x rotate to left by x degrees
+right:x rotate to the right by y degrees
+up:. pull the pen up
+down:. pull the pen down
+color:name change the color to name (use [string])
+begin-fill: begin filling
+end-fill: end filling the shape
+circle:r draw a circle with radius of r
+goto:x;y change the x coordinate to x and y coordinate to y
+movex:dx add dx to the x coordinate value
+movey:dy add dy to the y coordinate value
+write:s print data from s
+jump:lbl jump to a label
+cond-jump:lbl jump only if @ is true (python true)
+expr:m;a;b sets @ to the result of evaluated expression, see expr command
+```
+
+## `expr` command:
+| `method; a; b` | python expression |
+|----------------|-------------------|
+| `+; a; b  `    | `a + b      `     |
+| `-; a; b  `    | `a - b      `     |
+| `*; a; b  `    | `a * b      `     |
+| `/; a; b  `    | `a / b      `     |
+| `>; a; b  `    | `a > b      `     |
+| `<; a; b  `    | `a < b      `     |
+| `=; a; b  `    | `a == b     `     |
+| `|; a; b  `    | `a or b     `     |
+| `&; a; b  `    | `a and b    `     |
+| `!; a     `    | `not a      `     |
+| `get; a   `    | `data[a]    `     |
+| `set; a; b`    | `data[a] = b`     |
+
+## File Structure:
+Code is split into instructions with newlines.<br>
+Instructions are split into a name and an argument with colons (`:`)<br>
+For some command the argument is split into smaller bits with semi-colons (`;`)<br>
+For comments use number symbol (`#`)<br>
+1. `optional data segment`<br>
+    Example:
+    ```
+    data:
+        name=$10
+        str=[string]Hello,\nWorld!
+    ```
+2. `code segment`<br>
+    Example:
+    ```
+    code:
+        forward:$100
+        left:$name
+    ```
+
+
+## Files:
+* `main.py` - the *Turtle Master 2200*
+* `example.tmi` - an example (works only with Turtle Master 2000)
+* `homework.tmi` - homework
+* `homework.tmi` - homework 2
---
+++ main.py
@@ -0,0 +1,215 @@
+import turtle as t
+import tkinter as tk
+import tkinter.simpledialog as tkmsgbox
+import sys
+import re
+
+VERSION_MAJ = 2000
+VERSION_MIN = 200
+
+# Main function.
+def run(instructions_):
+    data = {}
+    instructions = []
+
+    def run_arg(arg):
+        if arg[0:len("[string]")] == '[string]':
+            return arg[1:] \
+                .replace("\\n", "\n") \
+                .replace("\\t", "\t") \
+                .replace("\\r", "\r")
+        arg = arg.strip()
+        if arg[0] == '%':
+            if not arg[1:] in data:
+                print("No such data entry: " + argument + "!", file=sys.stderr)
+                instr_idx += 1
+            return data[arg[1:]]
+        elif arg[0] == '$': return float(arg[1:])
+        elif arg[0] == '=': return arg[1:]
+        elif arg[0] == '@': return data[" "]
+        else: raise KeyError("No such prefix: " + arg[0])
+    
+    # If no file provided, ask user for the instructions
+    if instructions_ is None:
+        # Ask for instructions.
+        root = tk.Tk()
+        root.withdraw()
+        path_str = tkmsgbox.askstring("Turtle Master " + str(VERSION_MAJ + VERSION_MIN), "Enter your code:")
+        root.destroy()
+
+        # Create all of the instructions.
+        instructions_ = path_str.replace("\n", " ").replace("\t", " ").split(" ")
+    else:
+        instructions_ = instructions_.replace("\t", " ")
+        code_lines = []
+        mode = ""
+        for line in instructions_.split("\n"):
+            line = re.sub(' +', ' ', line)
+            # print("uh,", "'" + line.strip() + "'")
+            if line.strip() == "data:":
+                mode = "data"
+            elif line.strip() == "code:":
+                mode = "code"
+            else:
+                if len(line.strip()) != 0 and line.strip()[0] == '#': continue
+                if mode == "data" and len(line.strip()) != 0:
+                    # print("DATA MODE LINE")
+                    entry = line.split("=")
+                    data[entry[0].strip()] = run_arg(entry[1])
+                elif mode == "code":
+                    # print("CODE MODE LINE")
+                    code_lines.append(line)
+        instructions = '\n'.join(code_lines).split("\n")
+
+    labels = {}
+    
+    
+
+    # print(instructions)
+
+    # Create a turtle.
+    pen = t.Pen()
+
+    data[' '] = False
+
+    if '__speed' in data: pen.speed(float(data['__speed']))
+
+    ## LABEL SEARCH
+    instr_idx = 0
+    while instr_idx < len(instructions):
+        i = instructions[instr_idx]
+
+        if len(i.strip()) == 0:
+            instr_idx += 1
+            continue
+
+        # Split the instruction with colons, the first is command, the second is the argument.
+        code = i.split(":")
+        command = code[0].strip()
+        argument = code[1].strip()
+
+        if command == 'label':
+            labels[argument] = instr_idx
+        instr_idx += 1
+
+    ## RUNTIME
+    instr_idx = 0
+    while instr_idx < len(instructions):
+        i = instructions[instr_idx]
+        if len(i.strip()) == 0:
+            instr_idx += 1
+            continue
+
+        # Split the instruction with colons, the first is command, the second is the argument.
+        code = i.split(":")
+        command = code[0].strip()
+        argument = code[1].strip()
+
+        # Why there are no switch statements in python?!
+        if command == 'left':
+            pen.left(run_arg(argument))
+        elif command == 'right':
+            pen.right(run_arg(argument))
+        elif command == 'up':
+            pen.up()
+        elif command == 'down':
+            pen.down()
+        elif command == 'forward':
+            pen.forward(run_arg(argument))
+        elif command == 'backward':
+            pen.backward(run_arg(argument))
+        elif command == 'color':
+            pen.color(argument)
+        elif command == 'begin-fill':
+            pen.begin_fill()
+        elif command == 'end-fill':
+            pen.end_fill()
+        elif command == 'circle':
+            pen.circle(run_arg(argument))
+        elif command == 'goto':
+            position = argument.split(';') # split the argument with semicolon, so we have x and y position.
+            x = run_arg(position[0])
+            y = run_arg(position[1])
+            pen.goto(x, y)
+        elif command == 'movex':
+            pen.goto(pen.xcor() + run_arg(argument), pen.ycor()) # change x by the argument.
+        elif command == 'movey':
+            pen.goto(pen.xcor(), pen.ycor() + run_arg(argument)) # change x by the argument.
+        elif command == 'heading': pen.seth(run_arg(argument))
+        elif command == 'set-font-size': data['__font_size'] = int(run_arg(argument))
+        elif command == 'set-font-name': data['__font_name'] = str(run_arg(argument))
+        elif command == 'set-font-form': data['__font_form'] = str(run_arg(argument))
+        elif command == 'set-speed': data['__speed'] = run_arg(argument)
+        elif command == 'print':
+            tmp = run_arg(argument)
+            fname = 'Arial'
+            fsize = 8
+            fform = 'normal'
+            if '__font_name' in data: fname = data['__font_name']
+            if '__font_size' in data: fsize = data['__font_size']
+            if '__font_form' in data: fform = data['__font_form']
+            f = (fname, fsize, fform)
+            pen.write(tmp, font=f)
+
+        elif command == 'jump':
+            instr_idx = labels[argument.strip()] - 1
+            continue
+        
+        elif command == 'label': pass
+
+        elif command == 'cond-jump':
+            if data[" "]: instr_idx = labels[argument.strip()] - 1
+
+        elif command == 'expr':
+            bits = argument.split(";")
+            method = bits[0].strip()
+            a = run_arg(bits[1])
+            b = None
+            if len(bits[2].strip()) != 0: b = run_arg(bits[2])
+
+            if   method == '>': data[" "] = a > b
+            elif method == '<': data[" "] = a < b
+            elif method == '=': data[" "] = a == b
+            elif method == '|': data[" "] = a or b
+            elif method == '&': data[" "] = a and b
+            elif method == '!': data[" "] = not a
+
+            elif method == '+': data[" "] = a + b
+            elif method == '-': data[" "] = a - b
+            elif method == '*': data[" "] = a * b
+            elif method == '/': data[" "] = a / b
+
+            elif method == 'set': data[a] = b
+            elif method == 'get': data[" "] = a
+
+            else:
+                print("No such method: '" + method + "', skipping (please note that the value is unchanged)", file=sys.stderr)
+
+        else:
+            print("Warning! Unknown command '" + command + "', skipping", file=sys.stderr)
+        instr_idx += 1
+        if instr_idx >= len(instructions): break
+
+    print("Finished! Click on window to close.")
+
+    # Make sure turtle doesn't close.
+    pen.getscreen().exitonclick()
+
+# If we run the script and not use it as a library
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == '--help':
+        print("Usage:")
+        print("python", __file__, " --file <.tmi file>", "   ->   Run file")
+        print("python", __file__, " --help", "   ->   Show help (you are looking at it now)")
+        print("python", __file__, " --verion", "   ->   Display version")
+
+    elif len(sys.argv) == 2 and sys.argv[1] == '--version':
+        print("Turtle Master " + str(VERSION_MAJ + VERSION_MIN))
+
+    elif len(sys.argv) == 3 and sys.argv[1] == '--file':
+        instruction_file = open(sys.argv[2], "r")
+        code = instruction_file.read()
+        run(code)
+        instruction_file.close()
+    else:
+        run(None)
